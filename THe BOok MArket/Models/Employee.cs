defa64f834030a0a95ef7a1ff2c54e75c07fa461
Mr. KT Simelane <u16302606@up.ac.za>
2019-08-16 01:22:35 +0200

    Sadebar Nav Test 1

--- THe BOok MArket/Models/Employee.cs
+++ THe BOok MArket/Models/Employee.cs
@@ -11,8 +11,7 @@ namespace THe_BOok_MArket.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.Web;
-
+    
     public partial class Employee
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -32,9 +31,7 @@ namespace THe_BOok_MArket.Models
         public Nullable<int> EmpTitle_ID { get; set; }
         public Nullable<int> EmpGender_ID { get; set; }
         public string ImageData { get; set; }
-
-        public HttpPostedFileBase ImageFile { get; set; }
-
+    
         public virtual Employee_Gender Employee_Gender { get; set; }
         public virtual Employee_Title Employee_Title { get; set; }
         public virtual User User { get; set; }
