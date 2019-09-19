6a823d437deca092461cf4972caaf6f3e25afb6d
unknown <User@INFRD-03.up.ac.za>
2019-09-19 14:14:25 +0200

    help second commit

--- THe BOok MArket/Models/Model.Designer.cs
+++ THe BOok MArket/Models/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\HP\Documents\School Work\2019\INF 370\INF 370\THe BOok MArket\Models\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\User\Desktop\inf370\THe BOok MArket\Models\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
